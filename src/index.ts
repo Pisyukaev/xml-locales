5534cac8d2c8848f32e6d86a05a788f4cb890baa
Vitaly Zakutny <stalker-52@mail.ru>
2023-08-21 22:56:47 +0300

    feat: add function to add a string in locales

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+import { commands } from './commands';
+
+yargs(hideBin(process.argv))
+	.scriptName('xml-locales')
+	.usage('$0 <cmd> [args]')
+	// @ts-ignore
+	.command(commands)
+	.help().argv;
+
