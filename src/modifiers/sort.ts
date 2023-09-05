ee8c065127d239288651086b1f5de5dcf3b03fa6
Vitaly Zakutny <stalker-52@mail.ru>
2023-09-06 00:25:03 +0300

    chore: replace function to sort by

--- src/modifiers/sort.ts
+++ src/modifiers/sort.ts
@@ -1,4 +1,21 @@
-import { sortBy } from '../utils/files';
+export function sortBy(
+	strElements: StringElement[],
+	direction: 'asc' | 'desc'
+) {
+	const { compare } = new Intl.Collator();
+
+	const compareFn = (a: StringElement, b: StringElement) => {
+		const isAsc = direction === 'asc';
+		const first = isAsc ? a : b;
+		const second = isAsc ? b : a;
+
+		return compare(first.key_name.toLowerCase(), second.key_name.toLowerCase());
+	};
+
+	const sortedElements = strElements.sort(compareFn);
+
+	return sortedElements;
+}
 
 export function sort({ direction }: { direction: 'asc' | 'desc' }) {
 	return function ({ jsonXml }: { jsonXml: XmlJson }) {
