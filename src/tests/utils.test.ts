ff6f730e075f154f8b0acf274812959b6a2e053b
Vitaliy Zakutniy <stalker-52@mail.ru>
2023-10-04 21:48:47 +0300

    chore: added test for helper functions

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, test } from 'vitest';
 
-import { getFilesFromDir } from '../utils/files';
+import { checkKeyExist, checkKeyValueExist, getFilesFromDir, replaceValue } from '../utils/files';
+import { REPLACED_STRING_ELEMENTS, STRING_ELEMENTS } from './mock/data/stringElements';
+
+
 
 describe('Get xml files path', () => {
 	test('Files in mock folder', async () => {
@@ -15,3 +18,57 @@ describe('Get xml files path', () => {
 		expect(paths).rejects.toThrow('Not all files have *.xml extension');
 	});
 });
+
+describe('checkKeyExist', () => {
+	test('has key', () => {
+		const hasKey = checkKeyExist('testKey1', STRING_ELEMENTS)
+
+		expect(hasKey).toBeTruthy()
+	})
+
+	test('has no key', () => {
+		const hasKey = checkKeyExist('testKey', STRING_ELEMENTS)
+
+		expect(hasKey).toBeFalsy()
+	})
+})
+
+describe('checkKeyValueExist', () => {
+	describe('key', () => {
+		test('key exist', () => {
+		const hasKey = checkKeyValueExist('testKey1', STRING_ELEMENTS)
+		expect(hasKey).toBeTruthy()
+		})
+
+		test('key no exist', () => {
+			const hasKey = checkKeyValueExist('someKey', STRING_ELEMENTS)
+			expect(hasKey).toBeFalsy()
+			})
+	})
+
+	describe('value', () => {
+		test('value exist', () => {
+			const hasKey = checkKeyValueExist('text1', STRING_ELEMENTS)
+			expect(hasKey).toBeTruthy()
+			})
+
+			test('value no exist', () => {
+				const hasKey = checkKeyValueExist('someValue', STRING_ELEMENTS)
+				expect(hasKey).toBeFalsy()
+				})
+	})
+})
+
+describe('replaceValue', () => {
+	test('replaced value', () => {
+		const replacedStrings = replaceValue(STRING_ELEMENTS, 'testKey1', 'newValue')
+
+		expect(replacedStrings).toMatchObject(REPLACED_STRING_ELEMENTS)
+	})
+
+	test('does not replace value', () => {
+		const replacedStrings = replaceValue(STRING_ELEMENTS, 'someKey', 'someValue')
+
+		expect(replacedStrings).toMatchObject(STRING_ELEMENTS)
+	})
+})
